d6ab718dfa9f63a591bd222a204ec832b419f85d
harsh <harshmishra9013@gmail.com>
2022-11-28 21:42:29 +0530

    initial commit

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,37 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+const FRONT_END_ADDRESSES_FILE =
+    "/home/harshmishra/block-learn/nextjs-smartcontract-lottery/constants/contractAddresses.json"
+
+const FRONT_END_ABI_FILE =
+    "/home/harshmishra/block-learn/nextjs-smartcontract-lottery/constants/abi.json"
+
+module.exports = async function () {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("updating front end ...")
+
+        updateContractAddresses()
+        updateAbi()
+    }
+}
+async function updateAbi() {
+    const raffle = await ethers.getContract("Raffle")
+    fs.writeFileSync(FRONT_END_ABI_FILE, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function updateContractAddresses() {
+    const chainId = network.config.chainId.toString()
+    const raffle = await ethers.getContract("Raffle")
+    const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
+    if (chainId in currentAddresses) {
+        if (currentAddresses[chainId].includes(raffle.address)) {
+            currentAddresses[chainId].push(raffle.address)
+        }
+    }
+    {
+        currentAddresses[chainId] = [raffle.address]
+    }
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+}
+
+module.exports.tags = ["all", "frontend"]
